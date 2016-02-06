190b721d870e3f56ea83a0ad1e481a56414c131a
yazuya <une_personne_libre3@softbank.ne.jp>
2016-02-06 15:30:34 +0900

    Initial Commit

---
+++ icon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  icon
+//
+//  Created by 佐々木　裕也 on 2016/02/06.
+//  Copyright © 2016年 佐々木　裕也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
